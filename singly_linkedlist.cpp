91f108dcec7fe2f341b05b55047f788386f3dac1
BilkisShakila <bilkisshakilaa@gmail.com>
2025-05-06 10:57:02 +0700

    SimpleLinkedList

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -1,3 +1,42 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+};
+
+class linkedlist
+{
+    Node *START;
+
+public:
+    linkedlist()
+    {
+        START + NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim ==START->noMhs))
+            {
+                cout << "\nDuplikasi nohs tidak diijinkan\n";
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+        }
+    }
+}

99b7b557a9f44f311839d4d9d4db64435c99ed11
BilkisShakila <bilkisshakilaa@gmail.com>
2025-05-06 11:08:47 +0700

    LinkedListMahasiswa

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -37,6 +37,29 @@ public:
             }
             nodeBaru->next = START;
             START = nodeBaru;
+            return;
         }
+
+        Node *previos = START;
+        Node *current = START;
+
+        while ((current != NULL) &&(nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previos = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previos->next = nodeBaru;
+        
     }
-}
+    bool listEmpaty()
+    {
+        return (START == NULL);
+    }
+};
